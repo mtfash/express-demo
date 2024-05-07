de72376957d63ffc68d173e6dfaa73e04e153064
Mustafa Shujaie <5601785+mtfash@users.noreply.github.com>
2024-05-07 15:38:42 +0400

    trigger deployment

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-
-
-// some dummy comment
-
-// another dummy comment
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
